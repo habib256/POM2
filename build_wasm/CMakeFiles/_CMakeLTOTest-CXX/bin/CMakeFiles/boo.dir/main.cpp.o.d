6203927429f20bc9413d0d71946020384ffaa805
VERHILLE Arnaud <gist974@gmail.com>
2026-05-27 22:45:52 +0400

    Remove obsolete build files and configuration for WASM project
    This commit deletes several files related to the build configuration and installation of the WASM project, including `cmake_install.cmake`, `CMakeCache.txt`, and various CPack configuration files. The removal of these files is part of a cleanup effort to streamline the build process and eliminate unnecessary artifacts. Additionally, all generated Makefiles and binary files associated with the previous build setup have been removed to ensure a clean state for future builds. This change does not affect the core functionality of the project but prepares the environment for a more efficient build process moving forward.

--- build_wasm/CMakeFiles/_CMakeLTOTest-CXX/bin/CMakeFiles/boo.dir/main.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/boo.dir/main.cpp.o: \
-  /Users/factory/src/POM2/build_wasm/CMakeFiles/_CMakeLTOTest-CXX/src/main.cpp
--- build_wasm/CMakeFiles/pom2_imgui.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for pom2_imgui.
-# This may be replaced when dependencies are built.
